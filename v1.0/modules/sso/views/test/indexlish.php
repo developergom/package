dd96dd87c74bf3d88de9b77cd5263cb463e41f82
Soni <soniibrol@gmail.com>
2015-11-30 13:33:56 +0700

    reupload datatables example

---
+++ v1.0/modules/sso/views/test/indexlish.php
@@ -0,0 +1,23 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="box">
+			<div class="box-body">
+				<table class="table table-hover table-responsive testDataTable" cellspacing="0" width="100%">
+				    <thead>
+				        <tr>
+				            <th class="text-center" width="30%">Name</th>
+				            <th class="text-center" width="10%">Icon</th>
+				            <th class="text-center">Link/URL</th>
+				            <th class="text-center">Last Update</th>
+				        </tr>
+				    </thead>
+				    <tbody>
+				        <tr>
+				            <td align="center" colspan="4" class="dataTables_empty">No data</td>
+				        </tr>
+				    </tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
