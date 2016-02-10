e6cbcb633822cc5509c491e0a850daaa7889c288
Soni <soniibrol@gmail.com>
2016-02-10 17:04:16 +0700

    user modules sso

--- V1.0/modules/sso/controllers/Role.php
+++ V1.0/modules/sso/controllers/Role.php
@@ -9,8 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Role extends GN_Controller {
 
-    protected $models = ['role'];
+    protected $models = ['role','menu','action'];
     protected $helpers = [];
+    protected $return_type = 'array';
 
     public function __construct() {
         parent::__construct();
@@ -34,6 +35,18 @@ class Role extends GN_Controller {
                 'rules' => NULL
             ]
         ];
+    }
+
+    public function create() {
+        $this->load->helper('recursive');
+        $this->menu->as_array();
+
+        $recursive = data_recursive($this->menu->get_all(),'menu_id','menu_parent');
+        $this->data['menu'] = datagrid_recursive($recursive,'menu_name');
+        $this->action->as_array();
+        $this->data['action'] = $this->action->get_all();
+        
+        $this->view = 'sso/role/create';   
     }
 
 }
